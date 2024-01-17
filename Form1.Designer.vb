442991f97789548b15520acef8ddf7220453eef2
evansfundira <evansfundira@gmail.com>
2024-01-17 09:58:08 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,113 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Label1 = New Label()
+        Button2 = New Button()
+        NewBtn = New Button()
+        ListBox1 = New ListBox()
+        ComboBox1 = New ComboBox()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = SystemColors.ActiveCaption
+        Button1.Location = New Point(185, 83)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(95, 73)
+        Button1.TabIndex = 0
+        Button1.Text = "Click me"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(60, 110)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(72, 20)
+        Label1.TabIndex = 1
+        Label1.Text = "Click tthis"
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(196, 208)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(94, 29)
+        Button2.TabIndex = 2
+        Button2.Text = "Button2"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' NewBtn
+        ' 
+        NewBtn.BackColor = SystemColors.ActiveCaption
+        NewBtn.Location = New Point(374, 84)
+        NewBtn.Name = "NewBtn"
+        NewBtn.Size = New Size(95, 73)
+        NewBtn.TabIndex = 0
+        NewBtn.Text = "Click me"
+        NewBtn.UseVisualStyleBackColor = False
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.ItemHeight = 20
+        ListBox1.Items.AddRange(New Object() {"Lerato", "Tebogo", "Simiso"})
+        ListBox1.Location = New Point(575, 84)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(150, 104)
+        ListBox1.TabIndex = 3
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Items.AddRange(New Object() {"Evans", "Sinazo", "Nondumiso"})
+        ComboBox1.Location = New Point(574, 232)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(151, 28)
+        ComboBox1.TabIndex = 4
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(ComboBox1)
+        Controls.Add(ListBox1)
+        Controls.Add(Button2)
+        Controls.Add(Label1)
+        Controls.Add(NewBtn)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents NewBtn As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents ComboBox1 As ComboBox
+End Class
